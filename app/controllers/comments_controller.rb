02d2da3fa8d2ab712ca9f3c486c03ce9fd3a3b0b
bounceflick <scullingnull@gmail.com>
2015-04-05 16:14:32 -0700

    added post route for create comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,13 @@ class CommentsController < ApplicationController
   def index
     render json: Comment.first, root: false
   end
+
+  def create
+    comment = Comment.new(params)
+    if comment.save
+      render json: comment, status: 200
+    else
+      render :nothing, status: 403
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'first_post' => 'posts#index'
   get 'first_comment' => 'comments#index'
+
+  post 'comment' => 'comments#create'
 end
